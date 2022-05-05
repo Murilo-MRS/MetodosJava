327a79c0f20642193d527ea4e94690cc8e757f37
Murilo-MRS <murilosantana1993@gmail.com>
2022-05-05 18:14:12 -0300

    calculadora e main preparados

--- src/one/digitalinnovation/basecamp/Calculadora.java
+++ src/one/digitalinnovation/basecamp/Calculadora.java
@@ -1,4 +1,29 @@
 package one.digitalinnovation.basecamp;
 
 public class Calculadora {
+
+    public static void soma(double numero1, double numero2) {
+
+        double resultado = numero1 + numero2;
+
+        System.out.println("A soma de " + numero1 + " mais " + numero2 + " é " + resultado);
+    }
+    public static void subtracao(double numero1, double numero2) {
+
+        double resultado = numero1 - numero2;
+
+        System.out.println("A subtracao de " + numero1 + " menos " + numero2 + " é " + resultado);
+    }
+    public static void multiplicacao(double numero1, double numero2) {
+
+        double resultado = numero1 * numero2;
+
+        System.out.println("A multiplicacao de " + numero1 + " vezes " + numero2 + " é " + resultado);
+    }
+    public static void divisao(double numero1, double numero2) {
+
+        double resultado = numero1 / numero2;
+
+        System.out.println("A divisao de " + numero1 + " por " + numero2 + " é " + resultado);
+    }
 }
--- src/one/digitalinnovation/basecamp/Main.java
+++ src/one/digitalinnovation/basecamp/Main.java
@@ -1,4 +1,17 @@
 package one.digitalinnovation.basecamp;
 
 public class Main {
+
+    public static void main(String[] args) {
+//Calculadora
+        System.out.println("Exercício calculadora");
+        Calculadora.soma(3, 6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7, 8);
+        Calculadora.divisao(5, 2.5);
+
+////        Mensagem
+//        System.out.println("Exercício mensagem");
+//        Mensagem.obtermensagem
+    }
 }
